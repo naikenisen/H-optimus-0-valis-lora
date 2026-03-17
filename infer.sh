25aa69c44d2c09df300bb000282e02b5c8c8791c
Isen Naiken <isen.naiken@nexomedis.com>
2026-03-17 12:28:16 +0100

    feat: add inference script for model predictions

---
+++ infer.sh
@@ -0,0 +1,20 @@
+#!/bin/ksh
+#$ -q gpu
+#$ -o result.out
+#$ -j y
+#$ -N ia2hl_foundation
+#$ -cwd
+
+cd $WORKDIR
+source /beegfs/data/work/imvia/in156281/H-optimus-0-valis-lora/venv/bin/activate
+module load python
+export PYTHONPATH=/work/imvia/in156281/H-optimus-0-valis-lora/venv/lib/python3.9/site-packages:$PYTHONPATH
+export TORCH_HOME=/beegfs/data/work/imvia/in156281/H-optimus-0-valis-lora/torch_cache
+export HF_HOME=/beegfs/data/work/imvia/in156281/H-optimus-0-valis-lora/hf_cache
+export HF_TOKEN=$(cat ~/.hf_token)
+cd $WORKDIR/H-optimus-0-valis-lora
+
+python inference.py \
+	--checkpoint "checkpoints/last.pt" \
+	--input "dataset/test/HES" \
+	--output_dir "predictions"
